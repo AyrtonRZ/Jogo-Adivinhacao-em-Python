de087ec55e6ff99bf381ccf3d5bcc9edcf89a70a
Ayrton M. Rodrigues <109232580+AyrtonRZ@users.noreply.github.com>
2023-05-12 18:00:24 -0300

    feat: adiciona proposta inicial do jogo

---
+++ jogoadivinhacao.py
@@ -0,0 +1,40 @@
+print("")
+print("######################################")
+print("#                                    #")
+print("#  Bem vido ao Jogo de adivinhacao   #")
+print("#                                    #")
+print("######################################")
+
+numSecreto = 42
+totalTentativa = 5
+rodada = 1
+
+while(rodada <= totalTentativa):
+  print("")
+  print("Tentativa: ", rodada, "de", totalTentativa)
+  chute_str = input("Digite seu numero: ")
+  print("Voce digitou: ", chute_str)
+  #forma simplificada
+  #chute = int(input("Digite o seu numero: "))
+
+  #melhor forma
+  chute = int(chute_str)
+  acertou = chute == numSecreto
+  numMaior = chute > numSecreto
+  numMenor = chute < numSecreto
+
+  if(acertou):
+    print("Voce acertou!!!!")
+    print("PARABENS!!")
+  else:
+    if(numMaior):
+      print("Voce errou!, seu chute foi maior que o Secreto.")
+    elif(numMenor):
+      print("Voce errou!, seu chute foi menor que o Secreto.")
+
+  rodada = rodada +1
+  print("")
+  print("######################################")
+
+print("#           Fim do jogo              #")
+print("######################################")
